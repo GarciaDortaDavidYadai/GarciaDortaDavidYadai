f83c14f614461404a7b7f349cb641714c0af09be
Yadai David García Dorta <alu0101464145@ull.edu.es>
2021-12-04 21:29:49 +0000

    Equal_at_last_one

---
+++ Equal_at_last_one.cpp
@@ -0,0 +1,15 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2021-2022
+  *
+  * @file integer_division_and_reminder.cc
+  * @author Albert Einstein aeinstein@ull.es
+  * @date Feb 30 2022
+  * @brief The program reads two natural numbers a and b, with b > 0, and prints 
+  *        the integer division d and the remainder r of a divided by b.
+  *        By definition, d and r must be the only integer numbers such that 0=<r<b and db+r=a.
+  * @bug There are no known bugs
+  * @see https://jutge.org/problems/P48107
+  */
