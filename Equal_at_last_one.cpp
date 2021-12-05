c1271e90d6f39922fa0b58e35f02a2d1650f124e
Yadai David García Dorta <91839025+GarciaDortaDavidYadai@users.noreply.github.com>
2021-12-05 17:44:16 +0000

    Actualización

--- Equal_at_last_one.cpp
+++ Equal_at_last_one.cpp
@@ -4,12 +4,27 @@
   * Grado en Ingeniería Informática
   * Informática Básica 2021-2022
   *
-  * @file integer_division_and_reminder.cc
-  * @author Albert Einstein aeinstein@ull.es
-  * @date Feb 30 2022
+  * @file Equal_at_last_one.cc
+  * @author Yadai David García Dorta alu0101464145@ull.edu.es
+  * @date Dec 6 2021
   * @brief The program reads two natural numbers a and b, with b > 0, and prints 
   *        the integer division d and the remainder r of a divided by b.
   *        By definition, d and r must be the only integer numbers such that 0=<r<b and db+r=a.
   * @bug There are no known bugs
   * @see https://jutge.org/problems/P48107
   */
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include <Equal_at_last_one_programa.h>
+#include <Usage.h>
+
+int main () {
+
+  Usage(argc, argv);
+  RepiteUltimoDigito(usuario);
+  return 0;
+
+}
--- Equal_at_last_one_programa.h
+++ Equal_at_last_one_programa.h
@@ -5,15 +5,13 @@
 void RepiteUltimoDigito (int usuario) {
 
   std::cin >> usuario;
-  std::vector <int> vector0;
+  int* array= new int[usuario];
   std::vector <int> ContarCifras;
-  vector1 = [usuario];
-  ContarCifras = [0];
 
   for (int i = 0; i < usuario-1; i++) {
 
     ContarCifras [i] = 0;
-    std::cout << "El último número se ha repetido " << ContarCifras << " veces." << std::endl;
+    std::cout << ContarCifras << std::endl;
 
   }
 
