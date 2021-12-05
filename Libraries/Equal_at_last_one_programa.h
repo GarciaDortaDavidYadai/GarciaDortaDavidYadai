1f30cee05191e8e3e03a96955bcb71943b4e6435
Yadai David García Dorta <91839025+GarciaDortaDavidYadai@users.noreply.github.com>
2021-12-05 17:41:54 +0000

    Actualización

---
+++ Libraries/Equal_at_last_one_programa.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+void RepiteUltimoDigito (int usuario) {
+
+  std::cin >> usuario;
+  std::vector <int> vector0;
+  std::vector <int> ContarCifras;
+  vector1 = [usuario];
+  ContarCifras = [0];
+
+  for (int i = 0; i < usuario-1; i++) {
+
+    ContarCifras [i] = 0;
+    std::cout << "El último número se ha repetido " << ContarCifras << " veces." << std::endl;
+
+  }
+
+
+}
